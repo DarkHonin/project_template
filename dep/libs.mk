6776838cdc8d18a04730efecbc7b3cce54ce17b7
William GOURLEY <wgourley@e4r4p1.wethinkcode.co.za>
2018-08-21 11:17:35 +0200

    Making dependency management a thing

--- dep/libs.mk
+++ dep/libs.mk
@@ -1,5 +1,5 @@
 WORKING_DIR:=$(realpath .)
-FOLDERS=$(shell find $(LIBDIR) -name ft_lib)
+FOLDERS=$(shell find $(WORKING_DIR)/dep -name ft_lib)
 LIBRAIRIES=$(dir $(FOLDERS))
 INCLUDE=$(addprefix -I,$(addsuffix includes, $(LIBRAIRIES)))
 LINKS=$(foreach file,$(FOLDERS), $(addprefix -l,$(shell cat $(file))))
@@ -7,13 +7,20 @@ LINKS=$(foreach file,$(FOLDERS), $(addprefix -l,$(shell cat $(file))))
 mod_list:
 	@echo Installed modules:
 	@for i in $(LIBRAIRIES); do \
-		echo \t$$i;	\
+		echo '\t'$$i;	\
 	done
 
 mod_init:
 	@echo "Cloning submodules:"
 	@for i in $(shell cat $(WORKING_DIR)/dep/modules); do \
-		echo \t$$i;	\
+		echo '\t'$$i;	\
 		cd $(WORKING_DIR)/dep; \
 		git clone $$i -q; \
 	done
+
+mod_remove:
+	@if [ ! -z "$(MOD)" -a "$(MOD)" != " "  ]; then \
+		rm -rf $(WORKING_DIR)/dep/$(MOD); \
+	else \
+		echo "You need to define a mod: MOD=mod_name"; \
+	fi
