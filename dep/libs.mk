6354a38f94b8c947c895e54d543571cc8c90771c
William GOURLEY <wgourley@e4r4p1.wethinkcode.co.za>
2018-08-21 11:03:14 +0200

    git add things

--- dep/libs.mk
+++ dep/libs.mk
@@ -3,3 +3,17 @@ FOLDERS=$(shell find $(LIBDIR) -name ft_lib)
 LIBRAIRIES=$(dir $(FOLDERS))
 INCLUDE=$(addprefix -I,$(addsuffix includes, $(LIBRAIRIES)))
 LINKS=$(foreach file,$(FOLDERS), $(addprefix -l,$(shell cat $(file))))
+
+mod_list:
+	@echo Installed modules:
+	@for i in $(LIBRAIRIES); do \
+		echo \t$$i;	\
+	done
+
+mod_init:
+	@echo "Cloning submodules:"
+	@for i in $(shell cat $(WORKING_DIR)/dep/modules); do \
+		echo \t$$i;	\
+		cd $(WORKING_DIR)/dep; \
+		git clone $$i -q; \
+	done
